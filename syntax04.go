dcf15dc6c9c89e23dc23b7fcfd43054924279389
qqqaax <weq2998@gmail.com>
2021-09-19 21:33:10 +0900

    syntax04.go version06

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-//난수 추출된 수의 소수 판정 프로그램v0.5
+//난수 추출된 수의 소수 판정 프로그램v0.6
 
 func main() {
 	seed := time.Now().Unix()
@@ -14,13 +14,15 @@ func main() {
 
 	isPrime := true
 	number := rand.Intn(150) + 2
+	//number = 21
 	fmt.Println("임의로 추출된 수 : ", number)
 
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			isPrime = false
+			break // 첫 번쨰 약수가 발견되면 반복문 즉시 종료
 		}
-		fmt.Print(i, " ")
+		//fmt.Print(i, " ")
 	}
 
 	if isPrime { // 비교연산자 제거
